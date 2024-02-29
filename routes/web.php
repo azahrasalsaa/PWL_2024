24a25d0b8194713067d2a927a0cf35e587eb83cb
AzahraSalsaa <azahras751@gmail.com>
2024-02-29 21:53:10 +0700

    route js 2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +11,43 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/hello', function () {
+ return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+   
+Route::get('/', function () {
+    return 'Welcome';
+});
+
+Route::get('/about', function () {
+    return 'Name: Azahra Salsabila <br> NIM:2241720073';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'My name is '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function
+($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Article Page with ID '.$id;
+});
+
+Route::get('/user/{name?}', function ($name=null) {
+    return 'My name is '.$name;
+});
+    
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
 });
